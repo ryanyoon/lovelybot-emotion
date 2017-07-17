89851d92b3a24a8a05076b1a48b233608109546f
ryanyoon <ryanyoon0210@gmail.com>
2017-07-17 18:02:23 -0400

    Update tasks.py

--- tasks.py
+++ tasks.py
@@ -20,14 +20,14 @@ def echo_response(message):
 
   if message["type"] == "message":
     if "s8" and "capture" in message["text"]:
-
-      
+  
       msg = "A screenshot is a snapshot of your device screen saved as a photo. There are several ways to capture screenshots on your device. After the screenshots are captured, they will be automatically saved to the Gallery. If you want to know how to capture the screen on your galaxy S8 & S8+, click the link below."
       print(msg)
       msg2 = "http://www.samsung.com/us/support/answer/ANS00062596/"
       print(msg2)
       ReplyToActivity(fill=message, text=msg).send()
-      ReplyToActivity(fill=message, text=msg2).send()      
+      ReplyToActivity(fill=message, text=msg2).send() 
+      ReplyToActivity(fill=message, text=msg4).send()
     else:
       msg = "I'm sorry. Please enter the product name and keyword. For example, you can ask me like \"S8 screen capture\" or \"How can I capture my screen on my S8?\"."
       print(msg)
